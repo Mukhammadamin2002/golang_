221cc1c4ef5ed3127bbc015d2667faf472f47cf7
Mukhammadamin <72244353+Mukhammadamin2002@users.noreply.github.com>
2021-05-12 16:44:40 +0500

    Bar Chart Created

--- main.go
+++ main.go
@@ -1,112 +1,30 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"math/rand"
-	"net/http"
-	"strconv"
+	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/wcharczuk/go-chart"
 )
 
-// Books Structs (Model)
-
-type Book struct {
-	ID     string  `json:"id"`
-	Isbn   string  `json:"isbn"`
-	Title  string  `json:"title"`
-	Author *Author `json:"author"`
-}
-
-// Author struct
-
-type Author struct {
-	Firstname string `json:"firstname"`
-	Lastname  string `json:"lasttname"`
-}
-
-// Init Books var as a slice Book struct
-
-var books []Book
-
-// Get All Books
-
-func getBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/json")
-	json.NewEncoder(w).Encode(books)
-}
-
-func getBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/json")
-	params := mux.Vars(r) //Get Params
-	// Loop through books and find with id
-	for _, item := range books {
-		if item.ID == params["id"] {
-			json.NewEncoder(w).Encode(item)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(&Book{})
-}
-
-func createBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/json")
-	var book Book
-	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = strconv.Itoa(rand.Intn(10000000)) // mock Id not safe
-	books = append(books, book)
-	json.NewEncoder(w).Encode(book)
-}
-
-func updateBook(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			var book Book
-			_ = json.NewDecoder(r.Body).Decode(&book)
-			book.ID = params["id"]
-			books = append(books, book)
-			json.NewEncoder(w).Encode(book)
-			return
-		}
-	}
-	json.NewEncoder(w).Encode(books)
-}
-
-func deleteBooks(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content Type", "application/json")
-	params := mux.Vars(r)
-	for index, item := range books {
-		if item.ID == params["id"] {
-			books = append(books[:index], books[index+1:]...)
-			break
-		}
-	}
-	json.NewEncoder(w).Encode(books)
-}
-
 func main() {
-	// Init Router
-	r := mux.NewRouter()
-
-	// Mock Data
-	books = append(books, Book{ID: "1", Isbn: "3245", Title: "Book One", Author: &Author{
-		Firstname: "John",
-		Lastname:  "Doe",
-	}})
-	books = append(books, Book{ID: "2", Isbn: "3345", Title: "Book Two", Author: &Author{
-		Firstname: "Adam",
-		Lastname:  "Smith",
-	}})
-
-	// Route Handlers / Endpoints
-	r.HandleFunc("/api/books", getBooks).Methods("GET")
-	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	r.HandleFunc("/api/books", createBook).Methods("POST")
-	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
-	r.HandleFunc("/api/books/{id}", deleteBooks).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	graph := chart.BarChart{
+		Title: "World Pollution",
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top: 30,
+			},
+		},
+		Height:   256,
+		BarWidth: 50,
+		Bars: []chart.Value{
+			{Value: 65, Label: "1950"},
+			{Value: 50, Label: "1975"},
+			{Value: 75, Label: "1990"},
+			{Value: 69, Label: "2010"},
+			{Value: 80, Label: "2020"},
+		},
+	}
+	f, _ := os.Create("barchart.png")
+	defer f.Close()
+	graph.Render(chart.PNG, f)
 }
